e77ea7035db3b2358df5bd123d81dd8344e1a3fc
shashabella <136095445+BellaIssah@users.noreply.github.com>
2023-09-05 11:38:26 -0700

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Ayesha Issah and is
+        <a
+          href="https://github.com/shashabella/project1-react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
