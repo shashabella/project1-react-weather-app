ce8290ec1e26ca52fe1aaed4e5fa7527c61bc778
shashabella <136095445+BellaIssah@users.noreply.github.com>
2023-09-05 19:49:15 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Ayesha Issah and is
-        <a
-          href="https://github.com/shashabella/project1-react-weather-app"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Ayesha Issah and is
+          <a
+            href="https://github.com/shashabella/project1-react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
