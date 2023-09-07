4e3f7c7d52cfdf194b6d162cb332fb822acadd6a
shashabella <136095445+BellaIssah@users.noreply.github.com>
2023-09-06 17:27:14 -0700

    Styled the HTML

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was created by Ayesha Issah and is
           <a
             href="https://github.com/shashabella/project1-react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             {" "}
             open-sourced on Github
